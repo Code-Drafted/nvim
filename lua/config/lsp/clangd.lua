f7e08c2c68911d412462e39b61d5e6120864e7ce
MLTR <mikailoudem@gmail.com>
2026-01-19 17:04:32 +0100

    lsp change
    -new editions to c++ lsp so it is now able to tell if an header file can
    and should be included

--- lua/config/lsp/clangd.lua
+++ lua/config/lsp/clangd.lua
@@ -1,23 +1,24 @@
 local M = {}
 
 local clangd_cmd = {
-  "clangd",
-  "--background-index",
-  "--clang-tidy",
-  "--completion-style=detailed",
-  "--header-insertion=never",
-  "--header-insertion-decorators=0",
+	"clangd",
+	"--background-index",
+	"--clang-tidy",
+	"--completion-style=detailed",
+	"--header-insertion=never",
+	"--suggest-missing-includes",
+	"--all-scopes-completion",
+	"--header-insertion-decorators=0",
 }
 
 function M.setup(on_attach, capabilities)
-  vim.lsp.config("clangd", {
-    cmd = clangd_cmd,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	vim.lsp.config("clangd", {
+		cmd = clangd_cmd,
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 
-  vim.lsp.enable("clangd")
+	vim.lsp.enable("clangd")
 end
 
 return M
-
